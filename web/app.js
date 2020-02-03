650592be1062997524553c413be6799494368fc0
PaoloCordioli <p.cordioli@hotmail.it>
2020-02-03 09:51:18 +0100

    Cambiata la mappa ed utlizzio di React.leaflet

--- web/app.js
+++ web/app.js
@@ -1,40 +1,41 @@
+const { Map: LeafletMap, TileLayer, Marker, Popup } = window.ReactLeaflet
+
 class App extends React.Component {
    constructor(props) {
       super(props);
       this.state = {
-         message: [],
+         message: []
       };
    }
 
-   componentDidMount = async () => {
+   componentDidMount33 = async () => {
       let obj = await fetch("http://localhost:8080/Map/messages").then(r => r.json());
       this.setState({ message: obj.message })
+      console.log(this.state.message)
+   }
 
-      let myMap = L.map('root').setView([41.53, 12.30], 5)
-
-      L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-         maxZoom: 18,
-         id: 'mapbox/streets-v11',
-         accessToken: 'pk.eyJ1IjoibGFjYXJwYSIsImEiOiJjazYwcDlsdGowOW85M2Zwa3hvY2pqYTdqIn0.gH_BOcRoYaTB1Y5CHERLpw'
-      }).addTo(myMap);
-
-
-      this.state.message.forEach(mes => {
-         let marker = L.marker([mes.lat, mes.lon]).addTo(myMap)
-         marker.bindPopup(mes.content)
-      })
-
-
-      myMap.on('click',(e) =>{
-         let marker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(myMap)
-         marker.bindPopup("ssa").openPopup();
-         });
+   click = (e) => {
+      console.log(e.latlng.lat + " " + e.latlng.lng)
    }
 
    render() {
+      const position = [45.51, 10.2]
+
       return (
          <div className="map">
+            <LeafletMap center={position} zoom={5} onClick={this.click}>
+               <TileLayer
+                  attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+               />
+               <Marker position={position}>
+                  <Popup>
+                     <span>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+              </span>
+                  </Popup>
+               </Marker>
+            </LeafletMap>
          </div>
       )
    }
